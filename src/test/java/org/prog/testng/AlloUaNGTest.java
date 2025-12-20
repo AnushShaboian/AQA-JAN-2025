3991a43c2612da64d21865413413912ae452657c
anysh <anysh.shaboyan2807@gmail.com>
2025-12-20 20:39:08 +0100

    homework test Allo.ua with NG

--- src/test/java/org/prog/testng/AlloUaNGTest.java
+++ src/test/java/org/prog/testng/AlloUaNGTest.java
@@ -1,7 +1,18 @@
 package org.prog.testng;
 
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 //TODO: create separate test-ng.xml file with this class only
 //TODO: move your selenium test to this class
@@ -9,49 +20,41 @@ import org.testng.annotations.Test;
 //TODO: Add assertion that goods name has > 0 length
 
 public class AlloUaNGTest {
+    private WebDriver driver;
 
-//    @BeforeClass
-//    public void beforeAllo() {
-//        System.out.println(">>>>>>>>>>ALLO.START");
-//    }
-//
-//    @AfterClass
-//    public void afterAllo() {
-//        System.out.println(">>>>>>>>>>ALLO.END");
-//    }
-//
-//    @BeforeMethod
-//    public void beforeMethod() {
-//        System.out.println("Test!<<<<<<<<<<<<<<<<<<<<");
-//    }
-//
-//    @AfterMethod
-//    public void afterMethod() {
-//        System.out.println("Test!<<<<<<<<<<<<<<<<<<<<");
-//    }
+    @BeforeSuite
+        private void setUp () {
+        driver = new ChromeDriver();
+    };
 
-    @Test
-    public void ngTest1() {
-        System.out.println("Hello Allo UA Test! 5");
+    @AfterSuite
+        private void tearDown () {
+        if (driver !=null){
+            driver.quit();
+        }
     }
 
-    @Test
-    public void ngTest2() {
-        System.out.println("Hello Allo UA Test 1!");
-    }
+        @Test
+        public void testAlloUa () throws InterruptedException {
+        driver.get("https://allo.ua/");
 
-    @Test
-    public void ngTest3() {
-        System.out.println("Hello Allo UA Test 2!");
-    }
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(30L));
+        WebElement elementSearch = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.name("search")));
+        elementSearch.click();
 
-    @Test
-    public void ngTest4() {
-        System.out.println("Hello Allo UA Test 3!");
-    }
+        elementSearch.sendKeys("Samsung Galaxy S23 Ultra 12/512Gb Green");
+        elementSearch.sendKeys(Keys.ENTER);
 
-    @Test
-    public void ngTest5() {
-        System.out.println("Hello Allo UA Test! 4");
+        WebDriverWait webDriverWait2 = new WebDriverWait(driver, Duration.ofSeconds(60L));
+        WebElement card = webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.className("v-btn--cart")));
+
+        Thread.sleep(5000);
+
+        WebElement firstPhone = driver.findElement(By.xpath("//a[contains(@class,'product-card__title')]"));
+            String text = firstPhone.getText();
+            Assert.assertFalse(text.isEmpty(), "String is null");
+            Assert.assertTrue(text.length() > 0, "String length = 0");
+            System.out.println(text);
+
     }
 }
