c4269522bed8a9f3be20f696348ca9c2aaa38c89
anysh <anysh.shaboyan2807@gmail.com>
2025-12-30 23:47:09 +0100

    homework API tests

---
+++ src/main/java/org/prog/dto/LocationDto.java
@@ -0,0 +1,15 @@
+package org.prog.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class LocationDto {
+    private String city;
+}
