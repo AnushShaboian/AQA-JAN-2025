0eb188dd121c403b205b82ef3617e2692d1f6a11
anysh <anysh.shaboyan2807@gmail.com>
2026-01-12 00:49:24 +0100

    homework Jenkins

--- src/test/java/org/prog/cucumber/CucumberRunner.java
+++ src/test/java/org/prog/cucumber/CucumberRunner.java
@@ -2,7 +2,10 @@ package org.prog.cucumber;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.prog.cucumber.alloua.SqlAlloUa;
 import org.prog.cucumber.alloua.WebAllloUa;
 import org.prog.cucumber.steps.SqlSteps;
@@ -10,23 +13,30 @@ import org.prog.cucumber.steps.WebSteps;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "org.prog.cucumber.alloua",
-        tags = "@allo"
+        tags = "@allo",
+        plugin = {"pretty",
+                "json:target/cucumber-reports/Cucumber.json",
+                "html:target/cucumber-report.html",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        }
 )
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 
     @BeforeSuite
-    public void setUp() throws ClassNotFoundException, SQLException {
-        WebAllloUa.driver = new ChromeDriver();
+    public void setUp() throws ClassNotFoundException, SQLException, MalformedURLException {
+        WebAllloUa.driver = getRemoteDriver();
         WebAllloUa.driver.manage().window().maximize();
         Class.forName("com.mysql.cj.jdbc.Driver");
         SqlAlloUa.connection =
-                DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "password");
+                DriverManager.getConnection("jdbc:mysql://mysql-db-1:3306/db", "user", "password");
     }
 
     @AfterSuite
@@ -35,5 +45,10 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
         if (WebAllloUa.driver != null) {
             WebAllloUa.driver.quit();
         }
+    }
+
+    public WebDriver getRemoteDriver() throws MalformedURLException {
+        return new RemoteWebDriver(
+                new URL("http://selenoid-selenoid-1:4444/wd/hub"), new ChromeOptions());
     }
 }
