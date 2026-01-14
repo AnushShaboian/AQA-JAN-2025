d6a3d94565ecb13cfedfabc2f7387c2737c9262f
anysh <anysh.shaboyan2807@gmail.com>
2026-01-14 14:15:54 +0100

    homework Jenkins14 (http://localhost:4444/wd/hub) (http://host.docker.internal:4444)

--- src/main/java/org/prog/util/WebDriverFactory.java
+++ src/main/java/org/prog/util/WebDriverFactory.java
@@ -19,7 +19,7 @@ public class WebDriverFactory {
                         new URL("http://localhost:4444/wd/hub"), remoteChrome());
             default:
                 return new RemoteWebDriver(
-                        new URL("http://localhost:4444/wd/hub"), remoteChrome());
+                        new URL("http://host.docker.internal:4444"), remoteChrome());
         }
     }
 
