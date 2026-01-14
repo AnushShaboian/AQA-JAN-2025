a54e3006831b3cd4ea25366a66705b23acc38083
anysh <anysh.shaboyan2807@gmail.com>
2026-01-14 16:30:07 +0100

    homework Jenkins22

--- src/test/java/org/prog/cucumber/CucumberRunner.java
+++ src/test/java/org/prog/cucumber/CucumberRunner.java
@@ -37,7 +37,7 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
                     DriverManager.getConnection("jdbc:mysql://mysql-db-1:3306/db", "user", "password");
         } else {
             SqlAlloUa.connection =
-                    DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "password");
+                    DriverManager.getConnection("jdbc:mysql://selenium-hub/db", "user", "password");
         }
     }
 
